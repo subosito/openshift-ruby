911f3a829e39a1d37bb25e9c80fd34b116b619b4
Alif Rachmawadi <subosito@gmail.com>
2013-04-23 09:35:55 +0700

    add ruby installer

---
+++ bin/ruby-installer.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+export RBENV_ROOT="${OPENSHIFT_DATA_DIR}.rbenv"
+export RBENV_INIT="${OPENSHIFT_DATA_DIR}rbenv-init.sh"
+export RUBY_VERSION="2.0.0-p0"
+
+if [ ! -d "${RBENV_ROOT}" ] ; then
+
+    curl https://raw.github.com/codecase/rbenv-installer/master/bin/rbenv-installer | bash
+
+    echo "gem: --no-rdoc --no-ri" > ${OPENSHIFT_DATA_DIR}.gemrc
+
+    echo '
+export RBENV_ROOT="${OPENSHIFT_DATA_DIR}.rbenv"
+
+if [ -d "${RBENV_ROOT}" ]; then
+  export PATH="${RBENV_ROOT}/bin:${PATH}"
+  eval "$(rbenv init -)"
+fi
+
+alias gem="gem --config-file ${OPENSHIFT_DATA_DIR}.gemrc"
+    ' > $RBENV_INIT
+
+    source $RBENV_INIT
+
+    rbenv install $RUBY_VERSION
+    rbenv global $RUBY_VERSION
+
+    gem install bundler
+fi
