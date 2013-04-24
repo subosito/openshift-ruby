fa06f240985c773874d2aa6abc12c9e90feedef2
Alif Rachmawadi <subosito@gmail.com>
2013-04-24 20:10:12 +0700

    expand sinatra to use inline templates and slim

--- app.rb
+++ app.rb
@@ -1,5 +1,29 @@
 class App < Sinatra::Base
+  set :inline_templates, true
+  set :slim, pretty: true
+
   get '/' do
-    "Ruby #{RUBY_VERSION}: #{ENV['OPENSHIFT_GEAR_DNS']}"
+    @title = 'Openshift Ruby (codecase)'
+    @gear_dns = ENV['OPENSHIFT_GEAR_DNS']
+    slim :home
   end
 end
+
+__END__
+
+@@ layout
+doctype html
+html lang="en"
+  head
+    meta charset="utf-8"
+    title= @title
+
+  body
+    h1= @title
+    == yield
+
+  footer
+    em= @gear_dns
+
+@@ home
+pre= "Ruby #{RUBY_VERSION}"
